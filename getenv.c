56f9c5bcc2a711a3eca0cc3237768e0d009f7919
Imarino4 <ignacio.martacos@gmail.com>
2022-07-28 07:59:21 -0700

    adding getenv.c

---
+++ getenv.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _getenv - gets an environemnt variable
+ * @name: name of variable
+ * Return: string with name
+ */
+
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	int i = 0, j = 0, c_len;
+	char *var;
+
+	c_len = strlen(name);
+	if (name == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		if (strncmp(environ[i], name, c_len) == 0)
+			var = environ[i];
+	}
+	return (var);
+}
